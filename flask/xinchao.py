af9e1e4317007edd85e3a0698ce823ee5ea872ff
hung222 <90371118+hung222@users.noreply.github.com>
2021-10-06 20:35:23 +0700

    Update xinchao.py
    sua doi lan nua

--- flask/xinchao.py
+++ flask/xinchao.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def xinchào():
     return 'xin chào mọi người!,nkkbavlnhczbxnbcdccctclsndavhnvl'
+
+@app.route('/ https://github.com/hung222/tapflask/blob/nktbbc/flask/ccnkt.html")
+def ccnkt.hhtml():
+     return 'https://github.com/hung222/tapflask/blob/nktbbc/flask/ccnkt.html'      
  
 if __name__ == '__main__':
     app.run()
